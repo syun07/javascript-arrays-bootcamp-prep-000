dd438837211d0344039329d028563ea400d77720
Sarah Yun <sarahyun7@gmail.com>
2018-11-21 04:09:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,9 +39,4 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length-1)
 }
-<<<<<<< HEAD
-=======
 
-
-
->>>>>>> df558467d2f13f20b0e818b2aca65ff5ca2a2726
