cebfc2f013b876af7d282ecd85911c9242945188
Sarah Yun <sarahyun7@gmail.com>
2018-11-21 04:09:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,8 +5,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return array
+  return array.unshift(element);
 }
 
 function addElementToEndOfArray(array, element) {
